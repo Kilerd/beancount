fd27c09a576c255ede544d20d4b531da67388518
Kilerd <blove694@gmail.com>
2020-10-14 00:53:49 +0800

    feat: parser do not ignore whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ mod tests {
     fn it_works() {
         let directive = Box::new(Directive::Open(NaiveDate::from_ymd(1970, 1, 1), (Account::Assets(vec![]))));
         let x = DirectiveExpressionParser::new()
-            .parse("1970-01-01 open Assets")
+            .parse("1970-01-01 open Assets:")
             .unwrap();
         assert_eq!(directive, x);
 
