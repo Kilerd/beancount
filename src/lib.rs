e931475d43c037249d6872f9732c2c18e30216ea
Kilerd <blove694@gmail.com>
2020-10-14 11:16:43 +0800

    refact:remove unnecessary parentheses

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,13 @@ mod tests {
     fn it_works() {
         let directive = Box::new(Directive::Open(
             NaiveDate::from_ymd(1970, 1, 1),
-            (Account::Assets(vec![])),
+            Account::Assets(vec![]),
         ));
         let x = DirectiveExpressionParser::new()
             .parse("1970-01-01 open Assets:")
             .unwrap();
         assert_eq!(directive, x);
+
 
         let x = DirectiveExpressionParser::new()
             .parse("1970-01-01")
