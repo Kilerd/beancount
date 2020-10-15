1b6bd4d591d0d7fc937692e4f820d849f11b219d
Kilerd <blove694@gmail.com>
2020-10-15 20:18:30 +0800

    refact: remove useless test code.

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,3 @@ use lalrpop_util::lalrpop_mod;
 pub mod error;
 pub mod models;
 lalrpop_mod!(#[allow(clippy::all)] pub parser);
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::add;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(2, add(1, 1));
-    }
-}
